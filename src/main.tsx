a72002a04be53711ee4e0b9329eff1eed64d0a79
bethmij <bethmij@gmail.com>
2024-08-25 10:29:44 +0530

    path changed

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import {RouterProvider, createBrowserRouter} from "react-router-dom";
 
 const router = createBrowserRouter([
     {
-        path: "/customer",
+        path: "/",
         element: <CustomerForm />
     }
 ]);
